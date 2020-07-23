bfa47881c61683d9577fe85cfa6fb77de39c4763
Zoe Kirsh <zoekirsh@gmail.com>
2020-07-23 22:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return (n + two)
 }
 
 var funkyFunction = function() {
